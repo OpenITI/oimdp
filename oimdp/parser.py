0e773e1e66e617b97dd2e5589524defb774d3b48
raffazizzi <raffaeleviglianti@gmail.com>
2020-01-23 17:18:01 -0500

    Progress on parsing metatags, pages, paragraphs, verse, lines

--- oimdp/parser.py
+++ oimdp/parser.py
@@ -1,19 +1,86 @@
 import sys
-from .structures import Document
+import re
+from .structures import Document, PageNumber, Paragraph, Line, Verse
+
+# Container structure type for structure stack.
+PARA = "paragraph"
+
+# mARkdown tag library
+META = "#META#"
+METAEND = "#META#Header#End#"
+PAGE = "PageV"
+HEMI = "%~%"
+LINE = "~~"
+
+
+def parse_tags(s: str):
+    return s
+
+
+def parse_line(tagged_il: str):
+    text_only = tagged_il.replace("#", '').replace(HEMI, '').replace(LINE, '')
+
+    # remove line tag
+    il = tagged_il.replace(LINE, '')
+    # Hemistichs
+    # if (HEMI in il):
+    #     for (hemi in il.split(HEMI)):
+    #         # mark as hemi and process further by calling parse_tags()
+    return Line(il, text_only, il)
 
 
 def parser(text):
     """Parses an OpenITI mARkdown file and returns a Document object"""
     document = Document(text)
 
-    lines = text.splitlines()
+    # Split input text into lines
+    ilines = text.splitlines()
 
-    magic_value = lines[0]
+    # Magic value
+    magic_value = ilines[0]
 
     if magic_value.strip() != "######OpenITI#":
         raise Exception(
             "This does not appear to be an OpenITI mARkdown document")
         sys.exit(1)
 
-    document.set_MagicValue(magic_value)
-    print(document.magic_value)
+    document.set_magic_value(magic_value)
+
+    # Structure stack
+    current_structure = document
+
+    # RE patterns
+    para_pattern = re.compile("^#($|[^#])")
+    page_pattern = re.compile("PageV(\d+)P(\d+)")
+
+    # Input lines loop
+    for il in ilines:
+
+        # Non-machine readable metadata
+        if (il.startswith(META)):
+            if (il.strip() == METAEND):
+                continue
+            value = il.split(META, 1)[1].strip()
+            document.set_simple_metadata_field(il, value)
+
+        # Page numbers
+        elif (PAGE in il):
+            pv = page_pattern.search(il)
+            document.set_page_number(PageNumber(il, pv.group(1), pv.group(2)))
+
+        # Paragraphs and lines of verse
+        elif (para_pattern.search(il)):
+            first_line = parse_line(il)
+            if (HEMI in il):
+                # this is a verse line
+                document.set_verse(Verse(il, first_line))
+            else:
+                current_structure = Paragraph([first_line])
+                document.set_paragraph(current_structure)
+
+        # Paragraph lines
+        elif (il.startswith(LINE)):
+            if (isinstance(current_structure, Paragraph)):
+                current_structure.add_line(parse_line(il))
+
+    return document
--- oimdp/structures.py
+++ oimdp/structures.py
@@ -1,20 +1,104 @@
+from typing import List
+
+
 class MagicValue:
     """Magic Value of OpenITI mARkdown file"""
-    def __init__(self, value):
-        self.orig_value = value
+    def __init__(self, orig: str):
+        self.orig = orig
         self.value = "######OpenITI#"
 
     def __str__(self):
         return self.value
 
 
+class SimpleMetadataField:
+    """A non-machine readable metadata field"""
+    def __init__(self, orig: str, value: str):
+        self.orig = orig
+        self.value = value
+
+    def __str__(self):
+        return self.value
+
+
+class Line:
+    """A line of text"""
+    def __init__(self, orig: str, text_only: str, parts):
+        self.orig = orig
+        self.text_only = text_only
+        self.parts = parts
+
+    def __str__(self):
+        """returns the whole line without tags"""
+        return self.text_only
+
+
+class Verse:
+    """A line of poetry"""
+    def __init__(self, orig: str, line: Line):
+        self.orig = orig
+        self.line = line
+
+    def __str__(self):
+        """returns the whole line without tags"""
+        return self.line
+
+
+class Paragraph:
+    """A paragraph contaning a list of lines"""
+    def __init__(self, lines: List[Line]):
+        self.lines = lines
+
+    def add_line(self, line: Line):
+        self.lines.append(line)
+
+    def __str__(self):        
+        return "\n".join([str(l) for l in self.lines])
+
+
+class PageNumber:
+    """A page and volume number"""
+    def __init__(self, orig: str, vol: str, page: str):
+        self.orig = orig
+        self.page = page
+        self.volume = vol
+
+    def __str__(self):
+        return f"Vol. {self.volume}, p. {self.page}"
+
+
 class Document:
     """The OpenITI mARkdown document"""
     def __init__(self, text):
         self.orig_text = text
+        self.simple_metadata = []
+        self.content = []
 
-    def set_MagicValue(self, value):
-        self.magic_value = MagicValue(value)
+    def set_magic_value(self, orig: str):
+        self.magic_value = MagicValue(orig)
+
+    def set_simple_metadata_field(self, orig: str, value: str):
+        self.simple_metadata.append(SimpleMetadataField(orig, value))
+
+    def set_page_number(self, page: PageNumber):
+        self.content.append(page)
+
+    def set_paragraph(self, para: Paragraph):
+        self.content.append(para)
+
+    def set_verse_line(self, verse: Verse):
+        self.content.append(verse)
+
+    def get_clean_text(self, includeMetadata: bool = False):
+        text = ""
+        if (includeMetadata):
+            text += "Metadata:\n"
+            text += "\n".join([str(md) for md in self.simple_metadata])
+            text += "\n\n"
+
+        text += "\n".join([str(c) for c in self.content])
+
+        return text
 
     def __str__(self):
         return self.orig_text
